35b71b96d1f224a62c9edcecf5738c4c9f4a2a1f
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-10-06 01:54:10 +0300

    Write List Logged Calls Component tests
    #15

--- FrontEnd/src/features/list-logged-calls/list-logged-calls.component.spec.ts
+++ FrontEnd/src/features/list-logged-calls/list-logged-calls.component.spec.ts
@@ -1,19 +1,34 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
-
 import { ListLoggedCallsComponent } from './list-logged-calls.component';
 import { ListLoggedCallsModule } from './list-logged-calls.module';
 import { CallLoggerHttpClientService } from '../../services/call-logger-http-client.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('ListLoggedCallsComponent', () => {
   let component: ListLoggedCallsComponent;
   let fixture: ComponentFixture<ListLoggedCallsComponent>;
 
+  let callLoggerServiceSpy = jasmine.createSpyObj<CallLoggerHttpClientService>(['getLoggedCalls']);
+  callLoggerServiceSpy.getLoggedCalls.and.returnValue(of([]));
+
+  let routerSpy = jasmine.createSpyObj<Router>(['navigateByUrl']);
+  routerSpy.navigateByUrl.and.callThrough();
+
   beforeEach(async () => {
-    let callLoggerServiceSpy = jasmine.createSpyObj<CallLoggerHttpClientService>(['getLoggedCalls'])
 
     await TestBed.configureTestingModule({
       imports: [ListLoggedCallsModule],
-      providers: [{provide: CallLoggerHttpClientService, useValue: callLoggerServiceSpy}]
+      providers: [
+        {
+          provide: CallLoggerHttpClientService, 
+          useValue: callLoggerServiceSpy
+        },
+        {
+          provide: Router,
+          useValue: routerSpy
+        }
+      ]
     })
     .compileComponents();
 
@@ -24,5 +39,10 @@ describe('ListLoggedCallsComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call router navigateByUrl', () => {
+    component.gotoLogCall();
+    expect(routerSpy.navigateByUrl).toHaveBeenCalled();
   });
 });
