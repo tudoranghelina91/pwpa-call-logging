790994c40abed4fd1d31f2686a1f94f5084eb3e8
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-10-07 14:37:46 +0300

    Update FE tests
    - Include coverage
    
    #15

--- FrontEnd/karma.conf.js
+++ FrontEnd/karma.conf.js
@@ -30,10 +30,16 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ]
+      ],
+      check: {
+        global: {
+          statements: 80,
+          branches: 80,
+          functions: 80,
+          lines: 80
+        }
     },
     reporters: ['progress', 'kjhtml'],
-    // browsers: ['Chrome'],
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
       ChromeHeadlessNoSandbox: {
