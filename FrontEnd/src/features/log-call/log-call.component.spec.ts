fde551e5e6eaadb7bf3ac90bd3c44cdb55bb277d
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-10-07 14:17:09 +0300

    Increase coverage on Log Call Component
    #15

--- FrontEnd/src/features/log-call/log-call.component.spec.ts
+++ FrontEnd/src/features/log-call/log-call.component.spec.ts
@@ -3,7 +3,7 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LogCallComponent } from './log-call.component';
 import { CallLoggerHttpClientService } from '../../services/call-logger-http-client.service';
 import { LogCallModule } from './log-call.module';
-import { of, throwError } from 'rxjs';
+import { of } from 'rxjs';
 import { Router } from '@angular/router';
 
 describe('LogCallComponent', () => {
@@ -68,5 +68,51 @@ describe('LogCallComponent', () => {
 
     expect(callLoggerServiceSpy.logCall).toHaveBeenCalledTimes(1);
     expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['..']);
+  });
+
+  it('should recheck form validity on clearErrors', () => {
+    component.clearErrors('callerNameControl');
+    component.clearErrors('addressControl');
+    component.clearErrors('descriptionControl');
+
+    expect(component.isCallerNameValid).toBe(component.form.controls['callerNameControl'].valid);
+    expect(component.isAddressValid).toBe(component.form.controls['addressControl'].valid);
+    expect(component.isDescriptionValid).toBe(component.form.controls['descriptionControl'].valid);
+  });
+
+  it('should isCallerNameValid be true when callerNameControl is valid', () => {
+    component.form.controls['callerNameControl'].setValue('test');
+    component.clearErrors('callerNameControl');
+    expect(component.isCallerNameValid).toBeTrue();
+  });
+
+  it('should isCallerNameValid be false when callerNameControl is not valid', () => {
+    component.form.controls['callerNameControl'].setValue('');
+    component.clearErrors('callerNameControl');
+    expect(component.isCallerNameValid).toBeFalse();
+  });
+
+  it('should isAddressValid be true when addressControl is valid', () => {
+    component.form.controls['addressControl'].setValue('test');
+    component.clearErrors('addressControl');
+    expect(component.isAddressValid).toBeTrue();
+  });
+
+  it('should isAddressValid be false when addressControl is not valid', () => {
+    component.form.controls['addressControl'].setValue('');
+    component.clearErrors('addressControl');
+    expect(component.isAddressValid).toBeFalse();
+  });
+
+  it('should isDescriptionValid be true when descriptionControl is valid', () => {
+    component.form.controls['descriptionControl'].setValue('test');
+    component.clearErrors('descriptionControl');
+    expect(component.isDescriptionValid).toBeTrue();
+  });
+
+  it('should isdescriptionValid be false when descriptionControl is not valid', () => {
+    component.form.controls['descriptionControl'].setValue('');
+    component.clearErrors('descriptionControl');
+    expect(component.isDescriptionValid).toBeFalse();
   });
 });
