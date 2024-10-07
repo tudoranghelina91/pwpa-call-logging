2b705047ac386e195375106daa473a3792f29a54
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-10-07 15:05:32 +0300

    Rollback karma check
    #15

--- FrontEnd/karma.conf.js
+++ FrontEnd/karma.conf.js
@@ -30,15 +30,8 @@ module.exports = function (config) {
       reporters: [
         { type: 'html' },
         { type: 'text-summary' }
-      ],
-      check: {
-        global: {
-          statements: 80,
-          branches: 80,
-          functions: 80,
-          lines: 80
-        }
-    },
+      ]
+      },
     reporters: ['progress', 'kjhtml'],
     browsers: ['ChromeHeadlessNoSandbox'],
     customLaunchers: {
@@ -48,6 +41,5 @@ module.exports = function (config) {
       }
     },
     restartOnFileChange: true
-  }
   });
 };
