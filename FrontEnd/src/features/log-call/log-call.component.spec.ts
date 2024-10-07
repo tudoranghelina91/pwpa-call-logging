fbb04e31faa9679ebc284a7eb4cfb2e47938ee3b
Tudor Anghelina <16164903+tudoranghelina91@users.noreply.github.com>
2024-10-07 12:31:45 +0300

    Add Log call Component tests
    #15

--- FrontEnd/src/features/log-call/log-call.component.spec.ts
+++ FrontEnd/src/features/log-call/log-call.component.spec.ts
@@ -3,20 +3,37 @@ import { ComponentFixture, TestBed } from '@angular/core/testing';
 import { LogCallComponent } from './log-call.component';
 import { CallLoggerHttpClientService } from '../../services/call-logger-http-client.service';
 import { LogCallModule } from './log-call.module';
-import { of } from 'rxjs';
-import { Call, CallFormModel } from '../../models/Call';
+import { of, throwError } from 'rxjs';
+import { Router } from '@angular/router';
 
 describe('LogCallComponent', () => {
   let component: LogCallComponent;
   let fixture: ComponentFixture<LogCallComponent>;
 
-  let callLoggerServiceSpy = jasmine.createSpyObj<CallLoggerHttpClientService>(['logCall'])
-  let callFormModel = new CallFormModel('','','');
+  let routerSpy = jasmine.createSpyObj<Router>(['navigate']);
+  routerSpy.navigate.and.callThrough();
+  
+  let callLoggerServiceSpy = jasmine.createSpyObj<CallLoggerHttpClientService>(['logCall']);
+  callLoggerServiceSpy.logCall.and.returnValue(of(
+    {
+      callerName: '',
+      address: '',
+      description: ''
+    }));
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
       imports: [LogCallModule],
-      providers: [{provide: CallLoggerHttpClientService, useValue: callLoggerServiceSpy}]
+      providers: [
+        {
+          provide: CallLoggerHttpClientService, 
+          useValue: callLoggerServiceSpy
+        },
+        {
+          provide: Router,
+          useValue: routerSpy
+        }
+      ]
     })
     .compileComponents();
     
@@ -25,13 +42,31 @@ describe('LogCallComponent', () => {
     fixture.detectChanges();
   });
 
+  afterEach(async() => {
+    callLoggerServiceSpy.logCall.calls.reset();
+    routerSpy.navigate.calls.reset();
+  });
+
   it('should create', () => {
     expect(component).toBeTruthy();
   });
 
   it('should not log call when form invalid', () => {
     component.form.controls['callerNameControl'].setValue('');
+    fixture.detectChanges();
     component.submitCall();
     expect(callLoggerServiceSpy.logCall).toHaveBeenCalledTimes(0);
+  });
+
+  it('should submit when form valid', () => {
+    component.form.controls['callerNameControl'].setValue('test');
+    component.form.controls['addressControl'].setValue('test');
+    component.form.controls['descriptionControl'].setValue('test');
+
+    fixture.detectChanges();
+    component.submitCall();
+
+    expect(callLoggerServiceSpy.logCall).toHaveBeenCalledTimes(1);
+    expect(routerSpy.navigate).toHaveBeenCalledOnceWith(['..']);
   });
 });
